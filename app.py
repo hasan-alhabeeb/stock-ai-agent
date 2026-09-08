c6c1f5247cde0494353cb0c2ef9fda3c4668b21c
hasan-alhabeeb <hasan.alhabeeb@gmail.com>
2026-09-08 14:48:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 st.set_page_config(page_title="Financial AI Agent", layout="wide")
 
 # إعداد API ومكافحة السجلات
-API_KEY = "AQ.Ab8RN6JUAtT-c_iafE-PypzKO5H79fDRlzts-RUwHfaEFr1ytA"
+API_KEY = st.secrets.get("GEMINI_API_KEY", "")
 client = genai.Client(api_key=API_KEY)
 
 LOG_FILE = "agent_recommendations_log.csv"
